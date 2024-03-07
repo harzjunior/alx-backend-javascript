eee8177d4cd5330f0bda57cdc55d97162435638e
Haruna Bah <harunjibs@gmail.com>
2024-03-07 10:01:50 +0100

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
 import ClassRoom from './0-classroom';
 
 /**
- * Generates an array of {@link ClassRoom}s with a specified size.
- * Returns an array of {@link ClassRoom}s.
+ * Generates an array of {@link ClassRoom}s instances of a specified size
+ * Returns an arr of {@link ClassRoom}s.
  */
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
