106042efc253469c918f6c528920e16333255e0d
Haruna Bah <harunjibs@gmail.com>
2024-03-07 09:53:43 +0100

    0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a class room.
+ */
+export default class ClassRoom {
+  /**
+   * Initializes a new instance of the @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The max capacity for
+   * students in the class
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
