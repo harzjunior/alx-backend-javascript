41c371b81273fc200bd1dba2f3dd9aee65c18514
Haruna Bah <harunjibs@gmail.com>
2024-03-12 22:26:37 +0100

    10-update_uniq_items.js

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+/**
+ * Changes the amount of unique grocery items to 100.
+ * @param {Map<String, number>} map - A map of the name of a
+ * grocery and its quantity.
+ * @author Haruna Bah Jibril <https://github.com/harzjunior>
+ */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
