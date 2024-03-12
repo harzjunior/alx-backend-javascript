7a7ea40bc790f22a0d4e115941bbf3de00ad4d5c
Haruna Bah <harunjibs@gmail.com>
2024-03-12 22:02:15 +0100

    6-set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an arr.
+ * @param {Array} arr - The source arr.
+ * @author Haruna Bah Jibril <https://github.com/harzjunior>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
