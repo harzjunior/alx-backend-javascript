d54f123ee154c6af2142a47842bce2dc8894d272
Haruna Bah <harunjibs@gmail.com>
2024-04-30 23:58:39 +0100

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Shows a msg on the STDOUT.
+ * @param {String} msg to show.
+ * @author Haruna Bah Jibril <https://github.com/harzjunior>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
